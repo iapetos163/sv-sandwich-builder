10160d69100096f9a311dd07d0183ceee63dd88d
Brett Houtz <bcat38@gmail.com>
2022-12-13 20:53:19 -0500

    Vector generating functions

--- src/powers.ts
+++ src/powers.ts
@@ -40,6 +40,17 @@ export interface Power {
   level: number;
 }
 
+const getMealPowerVector = ({ mealPower, level }: Power, matchNorm: number) => {
+  let minNorm = 1;
+  if (level === 2) minNorm = 100;
+  if (level === 3) minNorm = 2000;
+  const norm = Math.max(minNorm, matchNorm);
+  return mealPowers.map((mp) => (mp === mealPower ? norm : 0));
+};
+
+const getTypeVector = (power: Power, matchNorm: number) =>
+  allTypes.map((t) => (t === power.type ? matchNorm : 0));
+
 const maxIngredients = 6;
 
 const maxSeasonings = 4;
