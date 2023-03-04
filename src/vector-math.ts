d4ba14939f6ddec14f231fd99ea2ca74161be6d9
Brett Houtz <bcat38@gmail.com>
2023-03-04 12:54:49 -0500

    Matrix transform function

--- src/vector-math.ts
+++ src/vector-math.ts
@@ -14,3 +14,12 @@ export const innerProduct = (a: number[], b: number[]) =>
   a.reduce((product, aVal, i) => aVal * (b[i] ?? 0) + product, 0);
 
 export const norm = (v: number[]) => Math.sqrt(innerProduct(v, v));
+
+/**
+ *
+ * @param m Matrix (array of M rows, each row with N components)
+ * @param v Vertical vector (N components)
+ * @returns Vector with M components
+ */
+export const applyTransform = (m: number[][], v: number[]) =>
+  m.map((u) => innerProduct(u, v));
