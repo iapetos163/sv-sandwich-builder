fb2fd5f9ef6f481bc09b04d64290bdf9f606cad5
Brett Houtz <bcat38@gmail.com>
2023-03-18 14:18:21 -0400

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
-}
+  testEnvironment: 'node',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteNameTemplate: (vars) =>
+          vars.filepath.replace(/(\/index)?\.test\.ts$/, ''),
+      },
+    ],
+  ],
+};
