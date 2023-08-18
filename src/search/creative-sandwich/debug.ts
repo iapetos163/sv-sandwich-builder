c94cb94383eaea3aff260ce537f252f4b3cc913d
Brett Houtz <bcat38@gmail.com>
2023-08-18 07:11:29 -0400

    Strip down creative sandwich

--- src/search/creative-sandwich/debug.ts
+++ src/search/creative-sandwich/debug.ts
@@ -1,15 +1,10 @@
-import { allTypes, mealPowerCopy } from '../../strings';
-import { Target, TargetConfig } from './target';
+import { mealPowerCopy } from '../../strings';
+import { Target } from './target';
 
 const flavorCopy = ['SWEET', 'SOUR', 'SALTY', 'BITTER', 'SPICY'];
 
-export const configSetToStr = (configs: TargetConfig[]) =>
-  `${configs[0].typeAllocation}
-mpPlaceIndices: ${configs.map((c) => c.mpPlaceIndex)}
-typePlaceIndices: ${configs.map((c) => c.typePlaceIndex)}`;
-
 export const targetToStr = (t: Target) =>
-  `${configSetToStr(t.configSet)}
+  `${t.typeAllocation}
 Boost ${
     t.boostPower !== null
       ? `${mealPowerCopy[t.boostPower]} (${flavorCopy[t.flavorProfile![0]]} + ${
