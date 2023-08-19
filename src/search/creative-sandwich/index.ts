dc4e247b6d7dcd2a1c954d0fb1a3d7a1f25ed2b0
Brett Houtz <bcat38@gmail.com>
2023-08-18 21:49:28 -0400

    Collect ingredients from solution

--- src/search/creative-sandwich/index.ts
+++ src/search/creative-sandwich/index.ts
@@ -7,8 +7,6 @@ import { solve } from 'yalps';
 import { ingredients } from '@/data';
 import { getModel } from './model';
 
-const SCORE_THRESHOLD = 0.2;
-
 export const emptySandwich = {
   fillings: [],
   condiments: [],
@@ -28,76 +26,45 @@ export const makeSandwichForPowers = (
     targetPowers,
     avoidHerbaMystica: true,
   });
-  const { sandwiches } = targets.reduce<{
-    sandwiches: Sandwich[];
-    leastScore: number;
-  }>(
-    ({ sandwiches, leastScore }, target) => {
-      const newSandwich = makeSandwichForTarget(
-        target,
-        leastScore * (1 + SCORE_THRESHOLD),
-      );
-      if (!newSandwich) return { sandwiches, leastScore };
-
-      const allSandwiches = [...sandwiches, newSandwich];
-      if (newSandwich.score >= leastScore) {
-        return {
-          sandwiches: allSandwiches,
-          leastScore,
-        };
-      }
-
-      const newMaxScore = newSandwich.score * (1 + SCORE_THRESHOLD);
-      return {
-        sandwiches: allSandwiches.filter((s) => s.score <= newMaxScore),
-        leastScore: newSandwich.score,
-      };
-    },
-    { leastScore: Infinity, sandwiches: [] },
-  );
-
+  const sandwiches = targets
+    .map((target) => makeSandwichForTarget(target))
+    .filter((s): s is SandwichResult => !!s);
   sandwiches.sort((a, b) => a.score - b.score);
   return sandwiches[0] ?? null;
 };
 
+type SandwichResult = {
+  score: number;
+  fillings: Ingredient[];
+  condiments: Ingredient[];
+};
+
 const makeSandwichForTarget = (
   target: Target,
-  maxScore: number,
   multiplayer = false,
-): Sandwich | null => {
-  type IngredientSelectionState = {
-    fillings: Ingredient[];
-    condiments: Ingredient[];
-    herba: Ingredient[];
-    skipIngredients: Record<string, boolean>;
-    baseMealPowerVector: number[];
-    typeVector: number[];
-    flavorVector: number[];
-    reachedAllTargets: boolean;
-    score: number;
-  };
-
-  const initialState: IngredientSelectionState = {
-    fillings: [],
-    condiments: [],
-    herba: [],
-    skipIngredients: {},
-    baseMealPowerVector: [],
-    typeVector: [],
-    flavorVector: [],
-    reachedAllTargets: false,
-    score: 0,
-  };
-
+): SandwichResult | null => {
   const model = getModel({ multiplayer });
 
   const solution = solve(model);
   if (solution.status === 'infeasible') return null;
 
-  solution.variables;
   if (solution.status === 'optimal') {
-    console.log(solution.variables);
-    return null; // TODO
+    const [, score] = solution.variables.find(([n]) => n === 'score') ?? [, 0];
+
+    const fillings: Ingredient[] = [];
+    const condiments: Ingredient[] = [];
+
+    solution.variables.forEach(([name, count]) => {
+      const ingredient = ingredients.find((i) => i.name === name);
+      if (!ingredient) return;
+      if (ingredient.ingredientType === 'filling') {
+        new Array(count).forEach(() => fillings.push(ingredient));
+      } else {
+        new Array(count).forEach(() => condiments.push(ingredient));
+      }
+    });
+
+    return { fillings, condiments, score };
   }
   throw solution;
 };
