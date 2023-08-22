b236d2ac6d1773eda91f6fd74a7c33c33449ab83
Brett Houtz <bcat38@gmail.com>
2023-08-21 21:27:41 -0400

    Switch to glpk.js and implement wrapper

---
+++ src/lp/index.ts
@@ -0,0 +1,69 @@
+import GLPK, { GLPK as GLPKInstance, LP } from 'glpk.js';
+
+const glpk: GLPKInstance = (GLPK as any)();
+
+export interface Model {
+  objective: {
+    direction: 'min' | 'max';
+    coefficients: Record<string, number>;
+  };
+  constraints: {
+    coefficients: Record<string, number>;
+    upperBound?: number;
+    lowerBound?: number;
+  }[];
+}
+
+export const transformModel = (model: Model): LP => {
+  const variableNames = new Set<string>();
+
+  const objective = {
+    direction:
+      model.objective.direction === 'min' ? glpk.GLP_MIN : glpk.GLP_MAX,
+    name: 'obj',
+    vars: Object.entries(model.objective.coefficients).map(([name, coef]) => {
+      variableNames.add(name);
+      return { name, coef };
+    }),
+  };
+
+  const subjectTo = model.constraints.map(
+    ({ coefficients, upperBound, lowerBound }, i) => {
+      let bnds = { type: -1, ub: 0, lb: 0 };
+      if (typeof upperBound === 'number' && typeof lowerBound === 'number') {
+        bnds = { type: glpk.GLP_DB, ub: upperBound, lb: lowerBound };
+      } else if (typeof upperBound === 'number') {
+        bnds = { type: glpk.GLP_UP, ub: upperBound, lb: 0 };
+      } else if (typeof lowerBound === 'number') {
+        bnds = { type: glpk.GLP_LO, ub: 0, lb: lowerBound };
+      }
+
+      const vars = Object.entries(coefficients).map(([name, coef]) => {
+        variableNames.add(name);
+        return { name, coef };
+      });
+
+      return { name: `cons${i}`, vars, bnds };
+    },
+  );
+
+  return {
+    name: 'LP',
+    objective,
+    subjectTo,
+    generals: Array.from(variableNames),
+  };
+};
+
+export const solve = (model: Model) => {
+  const transformedModel = transformModel(model);
+
+  const {
+    result: { vars, z, status },
+  } = glpk.solve(transformedModel);
+
+  return {
+    variables: vars,
+    objectiveValue: z,
+  };
+};
