5285095b561f46b3010be75866d00030a9c018a4
Brett Houtz <bcat38@gmail.com>
2023-08-24 19:19:20 -0400

    Remove type-vector

--- src/search/creative-sandwich/vector/index.ts
+++ src/search/creative-sandwich/vector/index.ts
@@ -1,11 +1,9 @@
-export { getTargetTypeVector, getMaxTypeVector } from './type-vector';
 export { getTargetFlavorVector } from './flavor';
 export {
   getTargetMealPowerVector,
   adjustMealPowerTargetForFlavorBoost,
 } from './meal-power';
 
-export type { GetTargetTypeVectorProps } from './type-vector';
 export type { GetTargetFlavorVectorProps } from './flavor';
 export type {
   GetTargetMealPowerVectorProps,
