5a1d3de9b8c30fca04a7114dcf712836d1bf4a03
Brett Houtz <bcat38@gmail.com>
2023-08-25 18:30:25 -0400

    Fixes to placement

--- src/search/creative-sandwich/target/placement.test.ts
+++ src/search/creative-sandwich/target/placement.test.ts
@@ -1,3 +1,4 @@
+import { MealPower, TypeIndex } from '@/enum';
 import { getTypeTargetsByPlace } from './placement';
 
 describe('getTypeTargetsByPlace', () => {
@@ -10,5 +11,28 @@ describe('getTypeTargetsByPlace', () => {
     expect(res[0]).toBe(0);
     expect(res[1]).toBe(null);
     expect(res[2]).toBe(null);
+  });
+
+  it('Handles a repeated typePlaceIndex where one power is typeless', () => {
+    const targetPowers = [
+      {
+        mealPower: MealPower.EGG,
+        type: TypeIndex.NORMAL,
+        level: 2,
+      },
+      {
+        mealPower: MealPower.TITLE,
+        type: TypeIndex.FAIRY,
+        level: 2,
+      },
+      {
+        mealPower: MealPower.RAID,
+        type: TypeIndex.NORMAL,
+        level: 2,
+      },
+    ];
+    const res = getTypeTargetsByPlace(targetPowers, [0, 0, 2]);
+
+    expect(res).toContain(TypeIndex.FAIRY);
   });
 });
