497e268763c9a1c88af5d574b329bf67141c7c7d
Brett Houtz <bcat38@gmail.com>
2023-09-26 18:05:32 -0400

    Account for dropped pieces in getPowersForIngredients

--- src/mechanics/sandwich.ts
+++ src/mechanics/sandwich.ts
@@ -1,10 +1,12 @@
+import { ingredients } from '@/data';
 import { Ingredient, ResultPower } from '@/types';
-import { add } from '@/vector-math';
+import { add, scale } from '@/vector-math';
 import { evaluateBoosts, isHerbaMealPower } from './powers';
 import { getBoostedMealPower, rankFlavorBoosts } from './taste';
 
 export const getPowersForIngredients = (
   ingredients: Ingredient[],
+  pieceDrops: Record<string, number>,
 ): ResultPower[] => {
   const init = {
     mealPowerBoosts: [] as number[],
@@ -12,14 +14,41 @@ export const getPowersForIngredients = (
     flavorBoosts: [] as number[],
   };
 
-  const { mealPowerBoosts, typeBoosts, flavorBoosts } = ingredients.reduce(
-    ({ mealPowerBoosts, typeBoosts, flavorBoosts }, ingredient) => ({
-      mealPowerBoosts: add(mealPowerBoosts, ingredient.baseMealPowerVector),
-      typeBoosts: add(typeBoosts, ingredient.typeVector),
-      flavorBoosts: add(flavorBoosts, ingredient.flavorVector),
-    }),
-    init,
+  const totalFillingPieces = ingredients
+    .filter((i) => i.ingredientType === 'filling')
+    .reduce((sum, ing) => sum + ing.pieces, 0);
+  const allowedDrops = Math.floor(totalFillingPieces / 2);
+  const totalDrops = Object.values(pieceDrops).reduce((sum, v) => sum + v, 0);
+  // TODO: handle two stars
+  if (totalDrops > allowedDrops) {
+    return [];
+  }
+
+  const ingredientsWithDrops = ingredients.concat(
+    Object.entries(pieceDrops).map(([id, numDropped]) => ({
+      ...ingredients.find((i) => i.id === id)!,
+      pieces: -numDropped,
+    })),
   );
+
+  const { mealPowerBoosts, typeBoosts, flavorBoosts } =
+    ingredientsWithDrops.reduce(
+      ({ mealPowerBoosts, typeBoosts, flavorBoosts }, ingredient) => ({
+        mealPowerBoosts: scale(
+          add(mealPowerBoosts, ingredient.baseMealPowerVector),
+          ingredient.pieces,
+        ),
+        typeBoosts: scale(
+          add(typeBoosts, ingredient.typeVector),
+          ingredient.pieces,
+        ),
+        flavorBoosts: scale(
+          add(flavorBoosts, ingredient.flavorVector),
+          ingredient.pieces,
+        ),
+      }),
+      init,
+    );
 
   const rankedFlavorBoosts = rankFlavorBoosts(flavorBoosts);
   const boostedPower = getBoostedMealPower(rankedFlavorBoosts);
